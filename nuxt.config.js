66bc675b22871eda085bfee8126c7c2f8cff52e8
Thomas William McClean <spam@tommcclean.me>
2019-12-17 21:31:57 +0000

    Update theme colour and title

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,11 +5,12 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'cmpct',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { name: 'theme-color', content: '#000000' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
