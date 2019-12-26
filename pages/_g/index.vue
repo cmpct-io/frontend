0df51e4cd5ed5c8eef980715a61560904deee70c
Thomas William McClean <spam@tommcclean.me>
2019-12-26 17:24:20 +0000

    Start working on the landing page

---
+++ pages/_g/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <container>
+    <h1>Ready to jump?</h1>
+    <p>The link you visited is an alias for a website, do you want to visit it?</p>
+    <p v-text="parameter" />
+  </container>
+</template>
+
+<script>
+import container from '@/components/layout/container.vue'
+
+export default {
+  components: {
+    container
+  },
+  computed: {
+    parameter () {
+      return this.$route.query.o
+    }
+  },
+  mounted () {
+    // Load Route
+
+    // Display Comments / Reports
+  }
+}
+</script>
