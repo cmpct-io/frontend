c8fd9f0a3a3e36632825b1c4c271f80314e5039a
Thomas William McClean <spam@tommcclean.me>
2019-12-30 21:25:22 +0000

    Add Time since labels

--- components/comments/comment.vue
+++ components/comments/comment.vue
@@ -2,13 +2,18 @@
   <div class="comment flex-container">
     <c-icon icon="user-circle" class="fa-fw mr" />
     <div class="content flex-grow">
-      <p v-text="name" class="bold" />
+      <p class="bold">
+        <span v-text="name" class="mr" />
+        <span v-text="timeSince" class="text-small" />
+      </p>
       <p v-text="commentText" />
     </div>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
+
 export default {
   props: {
     name: {
@@ -18,6 +23,15 @@ export default {
     commentText: {
       type: String,
       required: true
+    },
+    dateAdded: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    timeSince () {
+      return `(${moment(this.dateAdded).fromNow()})`
     }
   }
 }
