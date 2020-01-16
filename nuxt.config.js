2e55c090f60e2abc852b4f72605aa8cf883c406e
Thomas William McClean <spam@tommcclean.me>
2020-01-16 08:26:51 +0000

    Add Search Console Verification tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,8 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
-      { name: 'theme-color', content: '#000000' }
+      { name: 'theme-color', content: '#000000' },
+      { name: 'google-site-verification', content: 'Zih5ZaiWwv-IozG3zKriVzfzz9ZjMZ-OgoFraU0sABY' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
