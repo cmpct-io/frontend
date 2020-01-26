aa5eab0c62ac2bee86fea7239720cb3792d1c518
Thomas William McClean <spam@tommcclean.me>
2020-01-26 08:19:01 +0000

    Make input larger within generator

--- components/generator/input.vue
+++ components/generator/input.vue
@@ -120,7 +120,7 @@ export default {
 <style scoped lang="scss">
   input {
     background-color: transparent;
-    padding: 20px 20px;
+    padding: 20px 10px;
     border: 0;
     font-size: 1rem;
     outline: 0;
