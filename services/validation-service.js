a79a8dff3dd7713a17fd32999387c3a1301449aa
Thomas William McClean <spam@tommcclean.me>
2020-02-06 17:33:34 +0000

    Improve link validation

--- services/validation-service.js
+++ services/validation-service.js
@@ -1,5 +1,5 @@
-export const IS_VALID_URL = (text) => {
-  const expression = /[-a-zA-Z0-9@:%._~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_.~#?&//=]*)?/gi
-  const regex = new RegExp(expression)
-  return text.match(regex)
-}
+
+import urlRegex from 'url-regex'
+
+export const IS_VALID_URL = text =>
+  urlRegex({ exact: true, strict: false }).test(text)
