be2b8b68b4c069d8e3dc1170a654043a7660b109
Thomas William McClean <spam@tommcclean.me>
2020-02-09 08:15:54 +0000

    Remove Video link components

--- components/generator/generator.vue
+++ components/generator/generator.vue
@@ -14,7 +14,6 @@
       <submit-button />
     </form>
 
-    <video-link />
     <validation-warning />
   </div>
 </template>
@@ -29,15 +28,13 @@ import groupButton from '@/components/generator/group-button.vue'
 import pasteButton from '@/components/generator/paste-button.vue'
 import submitButton from '@/components/generator/submit-button.vue'
 import validationWarning from '@/components/generator/validation-warning.vue'
-import videoLink from '@/components/shared/video-link.vue'
 
 export default {
   components: {
     groupButton,
     pasteButton,
     submitButton,
-    validationWarning,
-    videoLink
+    validationWarning
   },
 
   data () {
